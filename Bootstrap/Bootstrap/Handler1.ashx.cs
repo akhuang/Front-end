b7e1058e31b64d6a821f604391724b1bb0e170b0
unknown <phoenix_huang@phoenix.keyou.corp>
2012-08-08 17:07:26 +0800

    Bootstrap Typeahead 2.1 source process

---
+++ Bootstrap/Bootstrap/Handler1.ashx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bootstrap
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+
+
+            System.Text.StringBuilder strValues = new System.Text.StringBuilder();
+
+            strValues.Append("[");
+
+            strValues.Append("{");
+            strValues.Append("\"id\":\"cust\",");
+            strValues.Append("\"label\":\"custName\"");
+            strValues.Append("}");
+            strValues.Append("]");
+            context.Response.ContentType = "application/json";
+            context.Response.ContentEncoding = System.Text.Encoding.UTF8;
+            context.Response.Write(strValues.ToString());
+            context.Response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
